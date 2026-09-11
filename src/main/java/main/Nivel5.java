895b7ae5bcf35667a457caa587263e6eb5e8325a
Victory949 <aaron.victory@ucr.ac.cr>
2026-09-10 21:35:01 -0600

    Finalización del ejercicio 17

--- src/main/java/main/Nivel5.java
+++ src/main/java/main/Nivel5.java
@@ -15,17 +15,23 @@ public class Nivel5
     //Ejercicio 17
     public void sumaArreglos()
     {
+        String total = "";
         int cantidades1[] = {2,4,6,8,10};
-        int cantidades2[] = {2,4,6,8,10};
+        int cantidades2[] = {1,3,7,9,13};
                 
         int cantidades3[] = new int[5];
 
-        for (int i = 0; i < cantidades1.length; i++)
+        //ciclo for para sumar cantidades
+        for (int i = 0; i < cantidades3.length; i++)
         {
             cantidades3[i] = cantidades1[i] + cantidades2[i];
         }//fin ciclo for
         
-        JOptionPane.showMessageDialog(null, "La suma de los arreglos cantidades1 y cantidades 2 es: "+cantidades3[5]);
+        for (int i = 0; i<cantidades3.length; i++)
+        {
+            total= total+"\n["+i+"] = "+cantidades3[i];
+        }
+        JOptionPane.showMessageDialog(null, "La suma de los arreglos cantidades1 y cantidades 2 es: "+total);
     }//fin metodo sumaArreglos
     
 }//fin clase Nivel5
